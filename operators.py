376ccb93e1570a34b0b1dd39e8bd172769a10c99
Najam-Ali <najamali041@gmail.com>
2025-06-28 18:50:24 +0500

    operater practice

--- operators.py
+++ operators.py
@@ -21,3 +21,29 @@ print(b) # its like print here are the b value that is 6+3 is printed
 d = 6> 3
 print(d)
 
+    # Logical operators
+
+# Logical operator is always retuen True and False value in answer. 
+
+e = True and False
+print(e)
+
+
+#  Truth table of the and operator
+
+print ("False and False is: ", False and False)
+print ("True and False is: ", True and False)
+print ("False and True is: ", False and True)
+print ("True and  True is: ", True and True)
+
+#  Truth table of the or operator
+
+print ("False  or False is: ", False or False)
+print ("True  or False is: ", True or False)
+print ("False  or True is: ", False or True)
+print ("True  or  True is: ", True or True)
+
+# Truth table of not operator
+    #it is only on one variable.
+print ("not False is: ", not False)
+print(not(True))
