c5ebb69ce2bd570ee650ef628621ef923fcb75b3
Najam-Ali <najamali041@gmail.com>
2025-06-28 18:37:40 +0500

    operater practice

---
+++ operators.py
@@ -0,0 +1,23 @@
+#  We have four type of operator in Phyton 
+        # 1: Assigment (=)
+        # 2: Arithmetic (+, -, *, /, %)
+        # 3: Comparison (==, !=, >, <, >=, <=) and answer is always a boolen value
+        # 4: Logical (and, or, not)
+
+a = 10 # its like assigment here are the a asssigned a value that is 10.
+b = 30 # its like assigment here are the b asssigned a value that is 30.
+c = a + b # its like arithmetic here are the c asssigned a value that is sum of a and b
+print (c) # its like print here are the c value that is sum of a and b is printed.
+
+a = 4-2 
+print(a) # its like print here are the a value that is 4-2 is printed
+b = 6
+# b += 3 # its like assigment here are the b asssigned a value that is 6+
+b -= 3 # its like decrement here are the b asssigned a value that is 6-3
+print(b) # its like print here are the b value that is 6+3 is printed
+
+    # Comparison 
+        # comaprison operator is always retuen True and False value in answer.
+d = 6> 3
+print(d)
+
