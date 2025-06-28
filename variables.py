2e10eb14a7de8bf43bce3ecbd67569ab8d3c190d
Najam-Ali <najamali041@gmail.com>
2025-06-28 16:47:50 +0500

    my variables file practise

---
+++ variables.py
@@ -0,0 +1,24 @@
+                                # Variables and thier write rules.
+
+# That are the valide way of write the variables.
+
+name = "ali"
+age = 20
+print("The name of student is ", name, "and his age is", age)
+                                # Data types in python
+
+# There are several data types in python, including:
+
+#a = 3           # 1. Integers: whole numbers, like 1, 2, 3,
+# b = 4.6         # 2. Floats: decimal numbers, like 3.14 or -0.5
+# c= "Najam Ali"  # 3. Strings: sequences of characters, like "hello" or 'hello'
+# d = True        # 4. Boolean: a logical value that can be either True or False
+# e = None      # 5. None: a special value that represents the absence of a value
+#Note "Variables do not need to be declared with any particular type, and can even change type after they have been set."
+# Casting
+# If you want to specify the data type of a variable, this can be done with casting.
+# a = int(3)
+# print (a)
+# Variables rules
+# if i want check the type of the operator i can use type() function
+# print(type(a))
