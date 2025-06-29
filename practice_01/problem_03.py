acaa5d76d6152b0d17456d2888e2c948f00d12eb
Najam-Ali <najamali041@gmail.com>
2025-06-29 15:27:12 +0500

    Solution of Problem_03

---
+++ practice_01/problem_03.py
@@ -0,0 +1,9 @@
+# Check the type of the variables assigned using input() funtion.
+
+# a = input("Enter the name: ")
+# print ("The type of variable",type(a))
+
+b = input("Enter your number:")
+print(type(b)) # This line shows you b has str type by default.
+t = int(b) # This line input functioN change the type of the b. new type of b is "int" that store in the variable t.
+print("The type of the variable t is ", type(t)) # Here you see your type is changed dur to use pf the type function int.
