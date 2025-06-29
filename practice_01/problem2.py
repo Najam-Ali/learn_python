e2357446c51f7a82ff7942e829a956dfe9b40765
Najam-Ali <najamali041@gmail.com>
2025-06-29 15:11:49 +0500

    problem_02 solution

---
+++ practice_01/problem2.py
@@ -0,0 +1,5 @@
+# Write a program to find reminder when a number is devided by Z.
+
+a = int(input("Enter a first num: "))
+b = int(input("Enter a second num: "))
+print("The reminder of two entered number is: ", a % b )
