2f0cff3da7037c1a1f81179e70099b00a0b9369b
Najam-Ali <najamali041@gmail.com>
2025-06-29 21:58:30 +0500

    Solution of Problem_04

---
+++ practice_01/problem_04.py
@@ -0,0 +1,5 @@
+# Use comparison operators to find out whether a given varibale "a" is greater then "b" or not.Take a = 34, b = 80.
+# I write the this program where user give input.
+a = input("Enter your first num: ")
+b = input("Enter ypur second num: ")
+print("Two number comparision that values", int(a) > int(b) )
