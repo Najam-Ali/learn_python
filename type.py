952680619a092e1e616e2980cbc314147c1fea7b
Najam-Ali <najamali041@gmail.com>
2025-06-29 12:53:20 +0500

    type function

---
+++ type.py
@@ -0,0 +1,20 @@
+# We find the type of the Data.
+# print(type(data)) # its a direct method to find the type of the data.
+# print(type(10))   # Example of the above line.
+
+a = 10
+data_type = type(a) # in this example the type of a ia store in data_type variable.
+# print(data_type)
+
+a = "harry"
+ 
+data_type = type(a) # in this example the type of a ia store in data_type variable.
+# print(data_type) # It will print the type that is string.
+
+                    # The conversion of a variable values type.Its also the casting of a variable value type.(type function)
+                      
+a = " 50.4"
+t = float(a) # type fuction (float, int, str)
+print(type(t)) # It will print the type that is string.
+
+
