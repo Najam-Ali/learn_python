c88ebfa9ba8743308a966b14dcb8174d6a7c1534
Najam-Ali <najamali041@gmail.com>
2025-07-01 10:08:15 +0500

    Slice postive string

---
+++ second_ch/string.py
@@ -0,0 +1,14 @@
+# String is a sequence of characters.When you witre in quates that is a string.
+a = 'Developer' # Single Quated 
+a = "Developer" # Double Quated 
+a = '''Developer''' #Triple Quated
+# String Slicing: A string in pyhton can be sliced for getting part of strings.(imputable means we can not any char change in a string if we want this then we have to create a new string e,g: "Harry" we want change r with p then create a new string for it.)
+
+lenght =  len(a) # It is show the lenght of the string.
+# print("The lenght of the string is: ", lenght)
+
+# The lenght of the string is started from 0(L to right) or -1(right to Left) that called the slicing of the string.
+ 
+lenght = a[0:3]
+# print("The short term of string os",lenght)
+
