90118b7970ee6f45baceb052f7bf2c21401db434
Najam-Ali <najamali041@gmail.com>
2025-07-02 23:34:56 +0500

    first chp of pyhton journey

---
+++ first_chp/input.py
@@ -0,0 +1,17 @@
+# Here in this chp we will discuss how a user wanted value input in a python program.
+
+# a = input("Enter your name: ")
+# print("Hello, " + a + "!"), # The out is (Hello, Your name!) why is it because the + operator concatinate the the string and the variable together.An other exapmle to see something differents
+
+# a = input("Enter your first number: ") # This will ask the user to enter a number like first input num,
+# b = input("Enter your second number: ") # This will ask the user to enter a number like second input num.
+# print(type(a)) # That is use for the type of the variable and verification
+# print(type(b)) # That is use for the type of the variable and verification
+# print("The sum of your two numbers is: ", a + b) # The out is (e.g: 1+2 = 12) its due to a + b is a string operation not a math operation and it concatinate the both number.
+
+# To solve this problem we can use int() function to convert the input to integer and then we can do the math operation.
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+print("The sum of your two numbers is: ", a + b) # This output is gives you the sum of the two number.
+# print ("The sum of your two numbers is: ", int(a) + int(b)) # Its an other way write if you want to convert the input to integer that are already string.Mean both operation perform in one line like conversion and sum operaton.
---
+++ first_chp/operators.py
@@ -0,0 +1,49 @@
+#  We have four type of operator in Phyton 
+        # 1: Assigment (=)
+        # 2: Arithmetic (+, -, *, /, %)
+        # 3: Comparison (==, !=, >, <, >=, <=) and answer is always a boolen value
+        # 4: Logical (and, or, not)
+
+a = 10 # its like assigment here are the a asssigned a value that is 10.
+b = 30 # its like assigment here are the b asssigned a value that is 30.
+c = a + b # its like arithmetic here are the c asssigned a value that is sum of a and b
+print (c) # its like print here are the c value that is sum of a and b is printed.
+
+a = 4-2 
+print(a) # its like print here are the a value that is 4-2 is printed
+b = 6
+# b += 3 # its like assigment here are the b asssigned a value that is 6+
+b -= 3 # its like decrement here are the b asssigned a value that is 6-3
+print(b) # its like print here are the b value that is 6+3 is printed
+
+    # Comparison 
+        # comaprison operator is always retuen True and False value in answer.
+d = 6> 3
+print(d)
+
+    # Logical operators
+
+# Logical operator is always retuen True and False value in answer. 
+
+e = True and False
+print(e)
+
+
+#  Truth table of the and operator
+
+print ("False and False is: ", False and False)
+print ("True and False is: ", True and False)
+print ("False and True is: ", False and True)
+print ("True and  True is: ", True and True)
+
+#  Truth table of the or operator
+
+print ("False  or False is: ", False or False)
+print ("True  or False is: ", True or False)
+print ("False  or True is: ", False or True)
+print ("True  or  True is: ", True or True)
+
+# Truth table of not operator
+    #it is only on one variable.
+print ("not False is: ", not False)
+print(not(True))
