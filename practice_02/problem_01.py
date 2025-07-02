f1a1d9a001d71e215a42996bbfe271011178558f
Najam-Ali <najamali041@gmail.com>
2025-07-02 14:44:49 +0500

    solution of problem_01

---
+++ practice_02/problem_01.py
@@ -0,0 +1,4 @@
+# Write a python program to display a user enterd name followed by Good Afternoon using input() function.
+
+name = input("Enter Your name:")
+print(f"Good Afternoon {name}") # It a way you access your variable value in your string you use find string like this
