902b85adcc49a1da2b3fa5728275f66ee281650f
Najam-Ali <najamali041@gmail.com>
2025-07-02 17:03:20 +0500

    Solution of promlem_03

---
+++ practice_02/problem_03.py
@@ -0,0 +1,6 @@
+# Write a program to detect Double spaces in a string
+
+str = "I am bad  boy"
+print("Find the double space of the string",str.find("  ")) 
+
+# Here we use a find method for the finding of the double spaces and it give the index of targeted char exist other it return the -1.
