05e07cd6a58d34ee5b8eefd014d9b07b8e08fa27
Najam-Ali <najamali041@gmail.com>
2025-07-04 00:37:03 +0500

    simple list details

---
+++ third_ch/list.py
@@ -0,0 +1,17 @@
+# List in the python
+
+# In pyhton programing we use the list to store any type of data in a variable like a container.Its like a array in other programming language.Sting is not editable means no change in a existed string but in list the the value can be change at any index.
+
+fruite = ["Apple", "Peach", 45, 78.7, False, "Ali", "Ahmed"]
+# print(fruite[1])
+
+fruite[1] = "Grapes" # It shows that the list are the muteable
+# print(fruite[1])
+
+# Here we see the some methods of list that similer to the string
+# print(fruite[1:5])  # it return this ['Grapes', 45, 78.7, False] similer to the 
+
+# Note In string when we aplly a function on a string it return a new value in result but In list change the original string with change means old list make a new list.
+
+
+
