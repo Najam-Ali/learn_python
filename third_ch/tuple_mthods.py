7f939c66b0c21839875b48211daa430f8468c3d1
Najam-Ali <najamali041@gmail.com>
2025-11-06 01:28:06 +0500

    I learn about the tuple method or operations

---
+++ third_ch/tuple_mthods.py
@@ -0,0 +1,17 @@
+# Here we discuse the methods or operations of the tuple.
+
+tup = (34,78,56,34,74,0,9)
+# print("The value at the indux[3] = ",tup[3]) # Accessing an element from the tuple by index.
+# print("The value at the indux[3] = ",tup[-3]) # Accessing an element from the tuple by negtive index.
+# There are the two main method of the tuple:
+# print(tup.count(34)) # it define as the how much time an element appear oor repeate in a tuple we use count( element )
+# print(tup.index(56) ) # Returns the index of the first occurrence of a value
+tup_1 = (0,1,2,3,4,5,6,7,8,9,10)
+tup_2 = (11,12,13,14,15)
+# concatinate_tup = tup_1 + tup_2 # This operation to Concatenation of Python Tuples, we will use plus operators(+).
+# nested_tup = (tup_1, tup_2) # A nested tuple in Python means a tuple inside another tuple.
+# repetition_tup = ((tup_1)*2)
+silicing_tup = ( 0,5,6,2,8,0,3,5) 
+# print(silicing_tup[1:]) 
+# print(silicing_tup[1:4]) 
+
