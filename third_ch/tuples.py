15b13381c602e836c96cf8ba45a36207e17c6cca
Najam-Ali <najamali041@gmail.com>
2025-11-05 13:52:08 +0500

    i learn about the tuple

---
+++ third_ch/tuples.py
@@ -0,0 +1,10 @@
+# When we need this type of phenomena in which we create a container and put some differents elements of different data types and not muteable then we use the concept of the Touple.
+
+# touple = ("Ali","Akbar",45,23,54,45,67,85) # Tuple look like this.
+
+# print(type(touple)) # its show the type of list called touple.
+
+old_tuple = () # it give the empty tuple and if we write a single ements in it i give a new data type not the tuple. Let take example:
+#example_tuple = (2) # It return the int type of this even include in the form of the tuple.But here we have a question how we can write a sigle element in tuple.Let take an other example:
+new_tuple = (3,) # It return the class type of the tuple.So its mean with this structure (element , ) use can create a tuple of a single element.The use o this " , " is to important in this situation. 
+print(type(new_tuple)) 
