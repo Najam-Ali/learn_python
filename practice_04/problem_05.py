0d6daa230828f6c86a42951f8fa857d4887242cb
Najam-Ali <najamali041@gmail.com>
2025-11-09 02:09:24 +0500

    This problem solved by myself

---
+++ practice_04/problem_05.py
@@ -0,0 +1,5 @@
+# Write a program to formate the following letter using escape sequence characters.
+# String = "Hi! NAJAM ALI.You can do it.Never give up.keep it up"
+
+formated_string = "Hi! NAJAM ALI.\n\tYou can do it.\nNever give up.\n\tkeep it up."
+print(formated_string)
