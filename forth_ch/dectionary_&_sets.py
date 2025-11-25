96209017a9769397b5b26f666d1c3ff48af80226
Najam-Ali <najamali041@gmail.com>
2025-11-25 12:24:01 +0500

    Concept of dictonary

---
+++ forth_ch/dectionary_&_sets.py
@@ -0,0 +1,19 @@
+# Sets & Dectionary also the way to store the data in memory through the pyhton.
+
+# => store the valuse with thier key value is called dictionary.
+
+student = {
+    "name" : "Ahmed",
+    "marks" : "50%",
+    "status" : "pass",
+    "class" : "Matric"
+}
+# print(student)
+# print(student["marks"]) # Its print the value againt this key.
+
+# Rule of the python.
+
+# 1: Dictionary is underordered.
+# 2: It is immutable.
+# 3: It is indexed.
+# 4: one key is not use more then one time.
