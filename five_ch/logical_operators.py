22bd23988866af88d26fb1c634960ef46f10b7ff
Najam-Ali <najamali041@gmail.com>
2025-12-20 01:56:32 +0500

    practice of local operators in condition statment

---
+++ five_ch/logical_operators.py
@@ -0,0 +1,50 @@
+# And operator
+
+# age = int(input("Enter your age: "))
+# gender = input("Enter your gender(M/F): ").upper()
+
+# if age>= 18 and gender == "M":
+#     print("You are eligible for room")
+# elif age >=18 and gender == "F":
+#     print("You are female,Sorry you are eligible for room")
+# else:
+#     print("Sorry!Dear you are below then 18.So, you'r not eligible for the room")
+
+
+# Or operator
+
+# a = 200
+# b = 33
+# c = 500
+# if a > b or a > c:
+#   print("At least one of the conditions is True")
+
+# a = 33
+# b = 200
+# if not a > b:
+#   print("a is NOT greater than b")
+
+# age = 25
+# is_student = False
+# has_discount_code = True
+
+# if (age < 18 or age > 65) and not is_student or has_discount_code:
+#   print("Discount applies!")
+
+# Membership Operators(in, not in)
+# fruits = ["apple", "banana"]
+# if "apple" in fruits:
+#     print("Apple exists")
+# if "mango" not in fruits:
+#     print("mango not found.")
+
+# Short-Hand If (One-Line If)
+# age = 23
+# if age >= 18: print("Adult!")
+
+# Ternary Conditional Operator
+# result = value_if_true if condition else value_if_false # SYNTAX
+# age = 16
+# status = "Adult" if age >= 18 else "Minor"
+# print(status)
+
