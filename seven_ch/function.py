2801c15bb6ed638d79df7577fbb3e821782d0a5b
Najam-Ali <najamali041@gmail.com>
2026-01-13 23:32:13 +0500

    Deination of function

---
+++ seven_ch/function.py
@@ -0,0 +1,14 @@
+
+# def avg(): # Its the defination of the function.
+#     sub1_mark = int (input ("Enter the marks of subject 1: "))
+#     sub2_mark = int (input ("Enter the marks of subject 2: ")) 
+#     sub3_mark = int (input ("Enter the marks of subject 3: "))
+#     avg = (sub1_mark + sub2_mark + sub3_mark) / 3
+#     print ("The average marks is: ", avg)
+# avg() # Here we call the function.And we can call it multiple times.
+
+# WAP a simple prgram greet a user with "Good Day" using function.
+def greet(): # Its the defination of the function.
+    print ("Good Day") # The body of the function.
+greet() 
+# Here we call the function.And we can call it multiple times.
